118733957b28a54a8a5a00b9fb0a5fdf5a3e6fb2
sreeja gubbala <gubbalasreeja884@gmail.com>
2026-07-19 17:04:28 +0530

    Remove SQL scripts from project root

--- analysis_queries.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    region,
-    SUM(sales) AS total_sales,
-    SUM(profit) AS total_profit
-FROM superstore
-GROUP BY region;
--- data_validation.sql
+++
@@ -1,5 +0,0 @@
-SELECT COUNT(*) FROM superstore;
-
-SELECT DISTINCT category FROM superstore;
-
-SELECT DISTINCT region FROM superstore;
