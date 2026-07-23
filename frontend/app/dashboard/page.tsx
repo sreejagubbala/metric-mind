91be0aef5e191e5bfd16991453a4b1096e51c140
IshitaGupta2004 <2304281530031@kashiit.ac.in>
2026-07-23 13:54:30 +0530

    Update page.tsx

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,41 +1,37 @@
-import Navbar from "../../components/Navbar";
+import Sidebar from "../../components/Sidebar";
 import MetricCard from "../../components/MetricCard";
 
 export default function Dashboard() {
   return (
-    <>
-      <Navbar />
+    <div
+      style={{
+        display: "flex",
+      }}
+    >
+      <Sidebar />
 
-      <div
+      <main
         style={{
+          flex: 1,
           padding: "40px",
         }}
       >
-        <h1
-          style={{
-            fontSize: "36px",
-            marginBottom: "30px",
-          }}
-        >
-          Dashboard
-        </h1>
+        <h1>Business Dashboard</h1>
 
         <div
           style={{
             display: "grid",
             gridTemplateColumns: "repeat(4,1fr)",
             gap: "20px",
+            marginTop: "30px",
           }}
         >
-          <MetricCard title="Revenue" value="₹ 12.5M" />
-
-          <MetricCard title="Profit" value="₹ 4.2M" />
-
+          <MetricCard title="Revenue" value="₹12.5M" />
+          <MetricCard title="Profit" value="₹4.2M" />
           <MetricCard title="Orders" value="21,560" />
-
           <MetricCard title="Margin" value="35%" />
         </div>
-      </div>
-    </>
+      </main>
+    </div>
   );
 }
