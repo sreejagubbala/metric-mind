9a2539e4b2a2e5dcf5803c499ecc539253233184
sreeja gubbala <gubbalasreeja884@gmail.com>
2026-07-24 20:25:22 +0530

    Refactor profit margin calculation using dbt macro

--- warehouse/metricmind_dbt/models/marts/metrics/profit_model.sql
+++ warehouse/metricmind_dbt/models/marts/metrics/profit_model.sql
@@ -3,7 +3,7 @@ SELECT
     category,
     SUM(sales) AS total_sales,
     SUM(profit) AS total_profit,
-    ROUND((SUM(profit) / NULLIF(SUM(sales), 0)) * 100, 2) AS profit_margin_percentage
+    {{ calculate_margin('profit', 'sales') }} AS profit_margin_percentage
 FROM {{ ref('fact_sales') }}
 GROUP BY
     region,
