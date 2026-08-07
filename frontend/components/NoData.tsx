9e6ab78d28e43097022aa059ca1de5ddb2f31285
IshitaGupta2004 <2304281530031@kashiit.ac.in>
2026-08-07 17:29:52 +0530

    Create NoData.tsx

---
+++ frontend/components/NoData.tsx
@@ -0,0 +1,33 @@
+export default function NoData(){
+
+return(
+
+<div
+
+style={{
+
+padding:"60px",
+
+textAlign:"center",
+
+color:"#888"
+
+}}
+
+>
+
+📊
+
+<h2>No Data Available</h2>
+
+<p>
+
+Upload business data to begin analysis.
+
+</p>
+
+</div>
+
+);
+
+}
