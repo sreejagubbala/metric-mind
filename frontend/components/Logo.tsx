e8fd2ad3f73ea495acd01c5abaa5780b36e37ede
IshitaGupta2004 <2304281530031@kashiit.ac.in>
2026-08-07 15:35:48 +0530

    Create  Logo.tsx

---
+++ frontend/components/Logo.tsx
@@ -0,0 +1,21 @@
+export default function Logo() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: "20px",
+      }}
+    >
+      <h1
+        style={{
+          color: "#2563eb",
+          fontSize: "38px",
+        }}
+      >
+        MetricMind
+      </h1>
+
+      <p>AI Powered Business Intelligence</p>
+    </div>
+  );
+}
